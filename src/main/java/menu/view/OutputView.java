a6239695dd105b2e9562402f9e641b728675136c
Sumi Chung <drsoombre@naver.com>
2024-12-06 16:48:38 +0900

    feat: 출력 형식 준수

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -15,11 +15,12 @@ public class OutputView {
         System.out.println("메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
         List<String> categories = manager.getCategoryNames();
-        System.out.printf("[ 카테고리 | %s | %s | %s | %s | %s ]", categories.get(0), categories.get(1), categories.get(2),
+        System.out.printf("[ 카테고리 | %s | %s | %s | %s | %s ]\n", categories.get(0), categories.get(1),
+                categories.get(2),
                 categories.get(3), categories.get(4));
         for (Coach coach : coaches.getCoaches()) {
             List<String> lunchMenuNames = coach.getLunchMenuNames();
-            System.out.printf("[ %s | %s | %s | %s | %s | %s ]", coach.getName(), lunchMenuNames.get(0),
+            System.out.printf("[ %s | %s | %s | %s | %s | %s ]\n", coach.getName(), lunchMenuNames.get(0),
                     lunchMenuNames.get(1), lunchMenuNames.get(2), lunchMenuNames.get(3), lunchMenuNames.get(4));
         }
     }
