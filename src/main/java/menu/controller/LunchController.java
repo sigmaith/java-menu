4bbb952e7eb9ff98443d7c62527318764d32d380
Sumi Chung <drsoombre@naver.com>
2024-12-06 14:19:58 +0900

    feat: 서비스 시작 안내

--- src/main/java/menu/controller/LunchController.java
+++ src/main/java/menu/controller/LunchController.java
@@ -23,7 +23,7 @@ public class LunchController {
         Menus menus = setMenus();
     }
 
-    public Menus setMenus() {
+    private Menus setMenus() {
         List<MenusByType> menusByTypes = new ArrayList<>();
         menusByTypes.add(new MenusByType(Category.japan, convertStringToList("규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼")));
         menusByTypes.add(new MenusByType(Category.korean, convertStringToList("김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음")));
@@ -31,6 +31,11 @@ public class LunchController {
         menusByTypes.add(new MenusByType(Category.asian, convertStringToList("팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜")));
         menusByTypes.add(new MenusByType(Category.western, convertStringToList("라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니")));
         return Menus.from(menusByTypes);
+    }
+
+    private Coaches getCoaches() {
+        outputView.printStart();
+
     }
 
     private List<String> convertStringToList(String input) {
