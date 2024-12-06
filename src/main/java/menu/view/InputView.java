a2439cb656a3919ebc6dc0b72694d011c85e6180
Sumi Chung <drsoombre@naver.com>
2024-12-06 16:58:21 +0900

    refactor: 고정 문구 상수화

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,7 +5,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import menu.controller.dto.MenuInfo;
 import menu.domain.Coach;
 import menu.domain.Coaches;
 import menu.domain.Menu;
@@ -14,14 +13,16 @@ import menu.exception.CustomException;
 import menu.exception.ErrorMessage;
 
 public class InputView {
+    private final String COACH_NAMES_INPUT_GUIDE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private final String EACH_COACH_PROHIBITED_MENUS_INPUT_GUIDE = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public Coaches getCoaches() {
-        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        System.out.println(COACH_NAMES_INPUT_GUIDE);
         return Coaches.from(readLine());
     }
 
     public List<Menu> getProhibitedMenusOf(Coach coach, Menus menus) {
-        System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.", coach.getName());
+        System.out.printf(String.format(EACH_COACH_PROHIBITED_MENUS_INPUT_GUIDE, coach.getName()));
         List<String> prohibits = Arrays.stream(readLine().split(",", -1)).collect(Collectors.toList());
         if (prohibits.size() < 0 || prohibits.size() > 2) {
             throw CustomException.from(ErrorMessage.PROHIBITED_MENUS_RANGE);
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,26 +6,32 @@ import menu.domain.Coaches;
 import menu.domain.Manager;
 
 public class OutputView {
+    private final String START_GUIDE = "점심 메뉴 추천을 시작합니다.";
+    private final String RESULT_GUIDE = "메뉴 추천 결과입니다.";
+    private final String DAY_OF_WEEK_GUIDE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    private final String CATEGORIES_OF_WEEK_GUIDE = "[ 카테고리 | %s | %s | %s | %s | %s ]\n";
+    private final String COACH_PERSONAL_MENUS = "[ %s | %s | %s | %s | %s | %s ]\n";
+    private final String TERMINATION_GUIDE = "추천을 완료했습니다.";
 
     public void printStart() {
-        System.out.println("점심 메뉴 추천을 시작합니다.");
+        System.out.println(START_GUIDE);
     }
 
     public void printLunchMenus(Manager manager, Coaches coaches) {
-        System.out.println("메뉴 추천 결과입니다.");
-        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+        System.out.println(RESULT_GUIDE);
+        System.out.println(DAY_OF_WEEK_GUIDE);
         List<String> categories = manager.getCategoryNames();
-        System.out.printf("[ 카테고리 | %s | %s | %s | %s | %s ]\n", categories.get(0), categories.get(1),
-                categories.get(2),
-                categories.get(3), categories.get(4));
+        System.out.printf(String.format(CATEGORIES_OF_WEEK_GUIDE,
+                categories.get(0), categories.get(1), categories.get(2),
+                categories.get(3), categories.get(4)));
         for (Coach coach : coaches.getCoaches()) {
             List<String> lunchMenuNames = coach.getLunchMenuNames();
-            System.out.printf("[ %s | %s | %s | %s | %s | %s ]\n", coach.getName(), lunchMenuNames.get(0),
-                    lunchMenuNames.get(1), lunchMenuNames.get(2), lunchMenuNames.get(3), lunchMenuNames.get(4));
+            System.out.printf(String.format(COACH_PERSONAL_MENUS, coach.getName(), lunchMenuNames.get(0),
+                    lunchMenuNames.get(1), lunchMenuNames.get(2), lunchMenuNames.get(3), lunchMenuNames.get(4)));
         }
     }
 
     public void printTermination() {
-        System.out.println("추천을 완료했습니다.");
+        System.out.println(TERMINATION_GUIDE);
     }
 }
